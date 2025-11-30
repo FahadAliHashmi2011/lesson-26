0b257999df8ef758c48f844cbce416a10c2d64b6
FahadAliHashmi2011 <faiezali2006@gmail>
2025-11-30 15:31:55 +0000

    d

---
+++ index.py
@@ -0,0 +1,10 @@
+class myClass:
+    __privateVar=27;
+    def __privMeth(self):
+        print("im inside class myClass")
+    def Hello(self):
+        print("private variable value",myClass.__privateVar)
+        foo= myClass()
+        foo.hello()
+        foo.__privMeth()
+
